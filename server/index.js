6eff7a34e63b7f57f5d83a56fafeb99b144d71b5
yashk21 <yashk8119@gmail.com>
2022-04-02 02:37:14 +0530

    Add Product Api Created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./conn/conn");
 const cors = require("cors");
 const User = require("./db/User");
+const Product = require("./db/Product");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -18,17 +19,30 @@ app.post("/register", async (req, res) => {
 });
 app.post("/login", async (req, res) => {
   // res.send(req.body);
-  console.log(req.body);
+  // console.log(req.body);
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      res.send(user);
+      res.status(200).json({
+        success: true,
+        data: user,
+      });
     } else {
-      res.send("Not Found");
+      // res.send("Not Found");
+      res.status(403).json({
+        success: false,
+        error: "not found",
+      });
+      // console.warn("not found");
     }
   } else {
-    res.send("All are required");
+    res.send("All fields are required");
   }
+});
+app.post("/add-product", async (req, res) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  res.send(result);
 });
 app.listen(5000, () => {
   console.log("Server connected");
