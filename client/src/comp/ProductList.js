c626cf4bfe9acfb29b916e177f5d7087e1078554
yashk21 <yashk8119@gmail.com>
2022-04-04 05:07:50 +0530

    delete, update and search api integrated

--- client/src/comp/ProductList.js
+++ client/src/comp/ProductList.js
@@ -21,10 +21,29 @@ const ProductList = () => {
       getProducts();
     }
   };
+  const searchhandle = async (e) => {
+    console.warn(e.target.value);
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
   // console.log("products", products);
   return (
     <div className="product-list">
       <h3>ProductList</h3>
+      <input
+        className="search-product-box"
+        type="text"
+        placeholder="Search Product"
+        onChange={searchhandle}
+      ></input>
       <ul>
         <li>S.No</li>
         <li>Name</li>
@@ -33,19 +52,23 @@ const ProductList = () => {
         <li>Comapny</li>
         <li>Operation</li>
       </ul>
-      {products.map((item, index) => (
-        <ul key={item._id}>
-          <li>{index + 1}</li>
-          <li>{item.name}</li>
-          <li>{item.price}</li>
-          <li>{item.category}</li>
-          <li>{item.company}</li>
-          <li>
-            <button onClick={() => deleteProduct(item._id)}>Delete</button>
-            <Link to={`/update/${item._id}`}>update</Link>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>{item.company}</li>
+            <li>
+              <button onClick={() => deleteProduct(item._id)}>Delete</button>
+              <Link to={`/update/${item._id}`}>update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No result found</h1>
+      )}
     </div>
   );
 };
--- client/src/comp/UpdateProduct.js
+++ client/src/comp/UpdateProduct.js
@@ -1,5 +1,98 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
 const UpdateProduct = () => {
-  return <div>UpdateProduct</div>;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [company, setCompany] = useState("");
+  const [error, setError] = useState(false);
+  const params = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+  const getProductDetails = async () => {
+    console.log(params);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`);
+    result = await result.json();
+    setName(result.name);
+    setCategory(result.category);
+    setCompany(result.company);
+    setPrice(result.price);
+  };
+  const updateProduct = async () => {
+    if (!name || !price || !category || !company) {
+      setError(true);
+      return false;
+    }
+    console.warn(name, price, category, company);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`, {
+      method: "put",
+      body: JSON.stringify({ name, price, category, company }),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    navigate("/");
+  };
+  return (
+    <div className="prodcut">
+      <h1>Update Product</h1>
+      <input
+        type="text"
+        className="inputBox"
+        value={name}
+        placeholder="Update product name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      {error && !name && (
+        <span className="inavlid-input">Enter valid name </span>
+      )}
+      <input
+        type="text"
+        value={price}
+        className="inputBox"
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+        placeholder="Update product price"
+      />
+      {error && !price && (
+        <span className="inavlid-input">Enter valid price </span>
+      )}
+      <input
+        type="text"
+        value={category}
+        className="inputBox"
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+        placeholder="Enter product category"
+      />
+      {error && !category && (
+        <span className="inavlid-input">Enter valid category</span>
+      )}
+      <input
+        type="text"
+        value={company}
+        className="inputBox"
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+        placeholder="Update product company name"
+      />
+      {error && !company && (
+        <span className="inavlid-input">Enter valid company name</span>
+      )}
+      <button onClick={updateProduct} className="submit">
+        Update Product
+      </button>
+    </div>
+  );
 };
 export default UpdateProduct;
